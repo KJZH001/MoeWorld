b66ff9ea0ae28144b9624f740bcf788e0b9eb06e
KJZH001 <2802007619@qq.com>
2024-10-05 10:57:50 +0800

    构建静态页面

---
+++ run_local_build.bat
@@ -0,0 +1,7 @@
+:start
+title [构建静态页面]萌界参考手册 by 晓空
+chcp 65001
+:main
+npm run docs:build
+:exit
+pause
---
+++ run_local_preview.bat
@@ -0,0 +1,7 @@
+:start
+title 萌界参考手册 by 晓空
+chcp 65001
+:main
+npm run docs:preview
+:exit
+pause
